618be219d3945002a36fd854b6582687520117ae
Alexandro Tapia <alexandrotapiaflores@gmail.com>
2020-02-10 19:05:45 -0500

    Add files via upload

--- src/About.js
+++ src/About.js
@@ -5,21 +5,112 @@ function About(options) {
 
     if(options.visible){
         return (
-            <section id="about"        
-            style={{
-                maxHeight:window.innerHeight*0.9,
-                minHeight:window.innerHeight*0.9,
-                maxWidth:window.innerWidth,
-                minWidth:window.innerWidth
-            }}>
+            <section id="about">
                 <div id="photoAbout">
                     <img src="./img/aboutPicture.jpg"/>
                 </div>    
+                <hr/>
                 <div id="contentAbout">
-                    <h1>Who am I?</h1>
-                    <p>I'm a web developer, software developer and Mechatronics Engineer graduated from the "Universidad de las Fuerzas Armadas - ESPE" from Ecuador. Throughout my profesional expirience I've worked for small and medium companies, providing services like networking, server maintenance, software development and web development. I've also worked for Ten-tech, where I was in charge of CAD modelling, PCB design and implementation and 3D printing.</p>
-                    <p>My passion is the software development, technology and IT, the industrial world and the teaching. I am characterized by always being constantly learning, being extremely self-taught and being hard-working, skills that have opened several professional doors for me.</p>
-                    <p>I've covered my habilities as follows. Also you can visit my LinkedIn profile and my PluralSight profile to check my scores in the different fields I've mentioned or you can check my GitHub profile, and see my personal projects, such as my own company LocalHost.</p>
+                    <h1 className="btnAbout" style={{textAlign:"right"}} onClick={()=>{
+                        if(document.getElementById('whoami').classList.contains("contentHidden")) {
+                            document.getElementById('whoami').removeAttribute("class")
+                            document.getElementById('whoami').setAttribute("class","contentAvailable")
+                        }
+                        else{
+                            document.getElementById("whoami").setAttribute("class","contentHidden")
+                        }
+                    }}>
+                        Who am I?&nbsp;&nbsp;&nbsp;&nbsp;
+                    </h1>
+                    
+                    <div id="whoami" className="contentHidden">
+                        <p>I'm a web developer, software developer and Mechatronics Engineer graduated from the "Universidad de las Fuerzas Armadas - ESPE" from Ecuador. Throughout my profesional expirience I've worked for small and medium companies, providing services like networking, server maintenance, software development and web development. I've also worked for Ten-tech, where I was in charge of CAD modelling, PCB design and implementation and 3D printing.</p>
+                        <p>My passion is the software development, technology and IT, the industrial world and the teaching. I am characterized by always being constantly learning, being extremely self-taught and being hard-working, skills that have opened several professional doors for me.</p>
+                        <p>I've covered my abilities as follows. Also you can visit my <a href="https://www.linkedin.com/in/andr%C3%A9s-tapia-a701aa193/" target="__blank">LinkedIn</a> profile and my <a href="https://app.pluralsight.com/profile/alexandro-tapia" target="__blank">PluralSight</a> profile to check my scores in the different fields I've mentioned or you can check my <a href="https://github.com/alexandro591/" target="__blank">GitHub</a> profile, and see my personal projects, such as my own company <a href="https://localhostecuador.netlify.com/" target="__blank">Localhost</a>.</p>
+                    </div>
+
+                    <h1 className="btnAbout" style={{textAlign:"right"}} onClick={()=>{
+                        if(document.getElementById('myskills').classList.contains("contentHidden")) {
+                            document.getElementById('myskills').removeAttribute("class")
+                            document.getElementById('myskills').setAttribute("class","contentAvailable")
+                        }
+                        else{
+                            document.getElementById("myskills").setAttribute("class","contentHidden")
+                        }
+                    }}>
+                        Skills&nbsp;&nbsp;&nbsp;&nbsp;
+                    </h1>
+
+                    <div id="myskills" className="contentHidden">
+                        <p>Front-end / Back-end developer</p>
+                        <p className="skills" style={{
+                            background: "linear-gradient(110deg, #fdcd3b 80%, #ffed4b 60%)"
+                        }}>80%</p>
+
+                        <p>software developer</p>
+                        <p className="skills" style={{
+                            background: "linear-gradient(110deg, #fdcd3b 100%, #ffed4b 60%)"
+                        }}>100%</p>
+
+                        <p>Mechanical knowledge</p>
+                        <p className="skills" style={{
+                            background: "linear-gradient(110deg, #fdcd3b 85%, #ffed4b 60%)"
+                        }}>85%</p>
+
+                        <p>Electrical and Electronics knowledge</p>
+                        <p className="skills" style={{
+                            background: "rgb(0,0,0)",
+                            background: "linear-gradient(110deg, #fdcd3b 95%, #ffed4b 60%)"
+                        }}>95%</p>
+
+                        <p>Networking and servers knowledge</p>
+                        <p className="skills" style={{
+                            background: "rgb(0,0,0)",
+                            background: "linear-gradient(110deg, #fdcd3b 90%, #ffed4b 60%)"
+                        }}>90%</p>
+                    </div>
+
+                    <h1 className="btnAbout" style={{textAlign:"right"}} onClick={()=>{
+                        if(document.getElementById('programs').classList.contains("contentHidden")) {
+                            document.getElementById('programs').removeAttribute("class")
+                            document.getElementById('programs').setAttribute("class","contentAvailable")
+                        }
+                        else{
+                            document.getElementById("programs").setAttribute("class","contentHidden")
+                        }
+                    }}>
+                        Programs I use&nbsp;&nbsp;&nbsp;&nbsp;
+                    </h1>
+
+                    <div id="programs" className="contentHidden">
+                        <p>SolidWorks</p>
+                        <p className="skills" style={{
+                            background: "linear-gradient(110deg, #fdcd3b 90%, #ffed4b 60%)"
+                        }}>90%</p>
+
+                        <p>Matlab</p>
+                        <p className="skills" style={{
+                            background: "linear-gradient(110deg, #fdcd3b 100%, #ffed4b 60%)"
+                        }}>100%</p>
+
+                        <p>Microsoft Office (Visio and Project included)</p>
+                        <p className="skills" style={{
+                            background: "linear-gradient(110deg, #fdcd3b 85%, #ffed4b 60%)"
+                        }}>85%</p>
+
+                        <p>FluidSIM</p>
+                        <p className="skills" style={{
+                            background: "rgb(0,0,0)",
+                            background: "linear-gradient(110deg, #fdcd3b 95%, #ffed4b 60%)"
+                        }}>95%</p>
+
+                        <p>CADe SIMU</p>
+                        <p className="skills" style={{
+                            background: "rgb(0,0,0)",
+                            background: "linear-gradient(110deg, #fdcd3b 90%, #ffed4b 60%)"
+                        }}>90%</p>
+                    </div>
+                    
                 </div>    
             </section>
         );
--- src/Resume.js
+++ src/Resume.js
@@ -5,9 +5,153 @@ function Resume(options) {
 
     if(options.visible){
         return (
-            <div className="main-app">
-                hey, this is resume
+            <div class="resume" id="resume"><br/>
+            <div class="container">
+              <div class="row">
+                <div class="col-md-6">
+                  <h3>My Experiences</h3>
+                  <ul class="timeline">
+                    <li>
+                      <h4>Ten-Tech (2013-2018)</h4>
+                      <p>PCB design and implementation.
+                        <br/>CAD design of mechanical elements.
+                        <br/>3D printing.
+                        <br/>Design and implementation of mechatronic projects, which implies the use of electrical and electronic elements, mechanical elements, and programming.
+                      </p>
+                    </li>
+                    <li>
+                      <h4>Consultorio jurídico Pérez Brito (2019)</h4>
+                      <p>Networking maintenance and implementation.
+                        <br/>Maintenance of the operating system and software instalation.
+                      </p>
+                    </li>
+                    <li>
+                      <h4>Notaría segunda del cantón Tena (2017-2019)</h4>
+                      <p>Networking maintenance and implementation.
+                        <br/>Maintenance of the operating system and software instalation.
+                        <br/>Adminitrative software development.
+                        <br/>Web development and social-media management.
+                        <br/>Design of the distribution and location drawings.
+                      </p>
+                    </li>
+                    <li>
+                      <h4>Universidad de las Fuerzas Armadas - ESPE (2018)</h4>
+                      <p>Design and construction of a social robot for the rehabilitation of autism children, using artificial vision and LabVIEW.
+                        <br/>Design and construction of a controlled ambient for social robots interaction.
+                      </p>
+                    </li>
+                    <li>
+                      <h4>Mecánica Automotriz Quito (2019)</h4>
+                      <p>Maintenance of stationary motors.
+                      </p>
+                    </li>
+                  </ul>
+                </div>
+    
+                <div class="col-md-6">
+                  <h3>Education</h3>
+                  <ul class="timeline">
+                    <li>
+                      <h4>Unidad Educativa "Amazónica" (2000-2006)</h4>
+                      <p>Primary School.
+                        <br/>Graduated.
+                        <br/><b>Location: </b>Napo - Tena
+                        <br/><b>Average: </b>19/20
+                      </p>
+                    </li>
+                    <li>
+                      <h4>Colegio Fisco-Misional “San José” (2006-2012)</h4>
+                      <p>Secondary School.
+                        <br/>Graduated.
+                        <br/><b>Location: </b>Napo - Tena
+                        <br/><b>Degree: </b>Físico - Matemático
+                        <br/><b>Average: </b>18/20
+                      </p>
+                    </li>
+                    <li>
+                      <h4>Universidad de las Fuerzas Armadas - ESPE (2013-2019)</h4>
+                      <p>University.
+                        <br/>Graduated.
+                        <br/><b>Location: </b>Sangolquí - Pichincha - Quito
+                        <br/><b>Degree: </b>Ingeniero en Mecatrónica
+                        <br/><b>Average: </b>17/20
+                      </p>
+                    </li>
+                  </ul>
+                </div>
+                <div class="col-md-6">
+                  <br/><h3>Courses and certifications</h3>
+                  <ul class="timeline">
+                    <li>
+                      <h4>CISCO - CCNA 1</h4>
+                      <p><b>Title: </b>Networking introduction.
+                        <br/><b>Place: </b>Sangolquí - ESPE Innovativa
+                        <br/><b>Date: </b>08/2019
+                        <br/><b>Hours: </b>40
+                      </p>
+                    </li>
+                    <li>
+                      <h4>CISCO - CCNA 2</h4>
+                      <p><b>Title: </b>Basic principles of routing and switching.
+                        <br/><b>Place: </b>Sangolquí - ESPE Innovativa
+                        <br/><b>Date: </b>08/2019
+                        <br/><b>Hours: </b>40
+                      </p>
+                    </li>
+                    <li>
+                      <h4>Neuromarketing seminar</h4>
+                      <p><b>Title: </b>introduction to neuromarketing.
+                        <br/><b>Place: </b>Sangolquí - ESPE Innovativa
+                        <br/><b>Date: </b>07/2019
+                        <br/><b>Hours: </b>8
+                      </p>
+                    </li>
+                    <li>
+                      <h4>ASME member</h4>
+                      <p><b>Title: </b>ASME member (American Society of Mechanical Engineers).
+                        <br/><b>Place: </b>Sangolquí - ASME ESPE
+                        <br/><b>Date: </b>07/2017
+                        <br/><b>Time as member: </b>1 year
+                      </p>
+                    </li>
+                  </ul>
+                </div>
+    
+                <div class="col-md-6">
+                  <br/><h3>Awards, honours and achievements</h3>
+                  <ul class="timeline">
+                    <li>
+                      <h4>Scientific Article</h4>
+                      <p><b>Title: </b>Pose Estimation Based on Monocular Visual Odometry and Lane Detection for Intelligent Vehicles.
+                        <br/><b>Function: </b>Co-author
+                        <br/><b>Link: </b><a href="https://sci-hub.tw/10.1007/978-3-319-95282-6_40" target="_blank">https://sci-hub.tw/10.1007/978-3-319-95282-6_40</a>
+                      </p>
+                    </li>
+                    <li>
+                      <h4>Contest</h4>
+                      <p><b>Title: </b>Smart University
+                        <br/><b>Idea: </b>“SAMI” (Sistema de asistencia multifuncional interactivo)
+                        <br/><b>Place: </b>Sangolquí - ESPE Innovativa
+                        <br/><b>Position: </b>Won in the category and best device in general.
+                      </p>
+                    </li>
+                    <li>
+                      <h4>Best Graduated</h4>
+                      <p><b>Title: </b>Best graduated from Mechatronics Engineering Faculty, promotion 201954
+                        <br/><b>Place: </b>ESPE - Sangolquí
+                      </p>
+                    </li>
+                    <li>
+                      <h4>CCNA 1 - 2 High scores</h4>
+                      <p><b>Title: </b>Award for culminate the studies of CNNA 1 and 2 with more than 90% of general score in both courses.
+                        <br/><b>Place: </b>Sangolquí - ESPE Innovativa
+                      </p>
+                    </li>
+                  </ul>
+                </div>
+              </div>
             </div>
+          </div>
         );
     }
     else{
